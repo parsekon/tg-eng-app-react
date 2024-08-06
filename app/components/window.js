0eabebfc317be137c55a08e9eecd7f693308aa11
DimaProCrypto <budanovdm@yahoo.com>
2024-08-05 23:34:27 -0700

    ready

--- app/components/window.js
+++ app/components/window.js
@@ -3,29 +3,33 @@ import { useEffect, useState } from "react";
 import ScriptsBlock from "../hook/useTelegram";
 
 const Tele = () => {
-    const [user, setUser] = useState()
-    useEffect(() => {
-        console.log('useTelegram')
-        function initTg() {
-        if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
-        console.log('Telegram WebApp is set');
-        const tgData = window.Telegram.WebApp
-        console.log(tgData)
-        setUser(tgData.initDataUnsafe?.user?.username)
-        } else {
-        console.log('Telegram WebApp is undefined, retrying…');
+  const [user, setUser] = useState();
+  useEffect(() => {
+    console.log("useTelegram");
+    function initTg() {
+      if (
+        typeof window !== "undefined" &&
+        window.Telegram &&
+        window.Telegram.WebApp
+      ) {
+        console.log("Telegram WebApp is set");
+        const tgData = window.Telegram.WebApp;
+        console.log(tgData);
+        setUser(tgData.initDataUnsafe?.user?.username);
+      } else {
+        console.log("Telegram WebApp is undefined, retrying…");
         setTimeout(initTg, 500);
-        }
-        }
-        initTg();
-        console.log(user)
-      }, []);
-    return (
-        <>
-        <ScriptsBlock />
-            <h1>{user}</h1>
-        </>
-    )
-}
+      }
+    }
+    initTg();
+    console.log(user);
+  }, []);
+  return (
+    <>
+      <ScriptsBlock />
+      <h1>{user}</h1>
+    </>
+  );
+};
 
-export default Tele
+export default Tele;
